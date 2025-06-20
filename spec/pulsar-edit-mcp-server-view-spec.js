d9b682762e02b589babfb6ce5add005e2f86cd47
Dustin Runnells <dustin@runnells.name>
2025-06-20 00:51:57 -0400

    Initial commit

---
+++ spec/pulsar-edit-mcp-server-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PulsarMcpView from '../lib/pulsar-edit-mcp-server-view';
+
+describe('PulsarMcpView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
